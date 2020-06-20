5225f9940d1eaad661e74913cc9f48623e0846c0
nicomiguelino <nicomiguelino2014@gmail.com>
2020-06-20 16:02:49 +0800

    Added first example

---
+++ examples/example_01.py
@@ -0,0 +1,24 @@
+from datetime import date
+from pprint import pprint
+
+import marshmallow
+
+class ArtistSchema(marshmallow.Schema):
+    name = marshmallow.fields.Str()
+
+class AlbumSchema(marshmallow.Schema):
+    title = marshmallow.fields.Str()
+    release_date = marshmallow.fields.Date()
+    artist = marshmallow.fields.Nested(ArtistSchema())
+
+def run():
+    bowie = { 'name': 'David Bowie' }
+    album = {
+        'artist': bowie,
+        'title': 'Hunky Dory',
+        'release_date': date(1971, 12, 17)
+    }
+
+    schema = AlbumSchema()
+    result = schema.dump(album)
+    pprint(result, indent=2)

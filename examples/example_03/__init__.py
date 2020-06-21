c8850324a78ea7ba4265bccaf0658ba0d80d3f48
nicomiguelino <nicomiguelino2014@gmail.com>
2020-06-21 10:26:31 +0800

    Added a post_load hook in deserializing Users

--- examples/example_03/__init__.py
+++ examples/example_03/__init__.py
@@ -8,7 +8,7 @@ from examples.schemas.user import UserSchema
 def run():
     """Deserializing Objects ("Loading")"""
     user_data = {
-        "created_at": "2014-08-11T05:26:03.869245",
+        "created_at": "2014-08-11 05:26:03.869245",
         "email": "ken@yahoo.com",
         "name": "Ken"
     }
@@ -16,4 +16,7 @@ def run():
     schema = UserSchema()
     result = schema.load(user_data)
 
+    print(f"type(user_data['created_at']): { type(user_data['created_at']) }")
     print(f"type(result): { type(result) }")
+    print(f"type(result.created_at): { type(result.created_at) }")
+    print(f"result: { result }")
--- examples/schemas/user.py
+++ examples/schemas/user.py
@@ -1,19 +1,24 @@
 import datetime as dt
 
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, post_load
 
 
 class User:
-    def __init__(self, name, email):
+    def __init__(self, name, email, **kwargs):
         self.name = name
         self.email = email
-        self.created_at = dt.datetime.now()
+        self.created_at = kwargs.get('created_at', dt.datetime.now())
 
     def __repr__(self):
-        return "<User(name={self.name!r})>".format(self=self)
+        return f"<User(name={ self.name } email={ self.email } " + \
+            f"created_at={ self.created_at })>"
 
 
 class UserSchema(Schema):
     name = fields.Str()
     email = fields.Email()
     created_at = fields.DateTime()
+
+    @post_load
+    def deserialize(self, data, **kwargs):
+        return User(**data)
